37b2b7df00342add0b04bf1af6e9170d626a1e69
Michel Pereira <michelbecauro@gmail.com>
2022-11-15 23:34:54 -0300

    FEAT: Add groups creations code

--- user_manager.sh
+++ user_manager.sh
@@ -5,3 +5,9 @@ echo "Creating directories...."
 mkdir /public && mkdir /adm && mkdir /ven && mkdir /sec
 
 echo "Directories created successfuly!"
+
+echo "Creating groups..."
+
+groupadd GRP_VEN && groupadd GRP_SEC && groupadd GRP_ADM
+
+echo "Groups created successfuly!"
