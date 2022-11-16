5097e60ad8802abff031b89b177f302f28561716
Michel Pereira <michelbecauro@gmail.com>
2022-11-16 00:37:52 -0300

    FEAT: Add 'permissions to folder' code

--- user_manager.sh
+++ user_manager.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-# echo "Creating directories...."
+echo "Creating directories...."
 
-# mkdir /public && mkdir /adm && mkdir /ven && mkdir /sec
+mkdir /public && mkdir /adm && mkdir /ven && mkdir /sec
 
-# echo "Directories created successfuly!"
+echo "Directories created successfuly!"
 
-# echo "Creating groups..."
+echo "Creating groups..."
 
-# groupadd GRP_VEN && groupadd GRP_SEC && groupadd GRP_ADM
+groupadd GRP_VEN && groupadd GRP_SEC && groupadd GRP_ADM
 
-# echo "Groups created successfuly!"
+echo "Groups created successfuly!"
 
-# echo "Creating users ..."
+echo "Creating users ..."
 
-# useradd -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM carlos
-# useradd -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM maria
-# useradd -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM joao
+useradd -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM carlos
+useradd -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM maria
+useradd -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_ADM joao
 
-# useradd debora -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
-# useradd sebastiana -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
-# useradd roberto  -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
+useradd debora -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
+useradd roberto  -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_VEN
 
-# useradd josefina -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
-# useradd amanda -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
-# useradd rogerio -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
+useradd josefina -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
+useradd amanda -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
+useradd rogerio -m -s /bin/bash -p $(openssl passwd Senha123) -G GRP_SEC
 
-# echo "Users created successfuly!"
+echo "Users created successfuly!"
 
 echo "Attributing groups to folders"
 
@@ -36,3 +36,14 @@ chown root:GRP_SEC /sec
 chown root:GRP_ADM /adm
 
 echo "Folders' groups defined successfuly"
+
+echo "Attributing permissions to folders"
+
+chmod 777 /public
+chmod 770 /ven
+chmod 770 /sec
+chmod 770 /adm
+
+echo "Folders' permissions defined successfuly"
+
+echo "THE END! bye :-)"
